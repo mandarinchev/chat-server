c42062182961f446e5f9f08f670d602ac8c0da08
Plamen Marinchev <marinchevp@gmail.com>
2025-02-15 10:08:37 +0200

    Remove unavailable dependency.

--- src/main/java/bg/fmi/chatserver/chat/spi/ChannelRepository.java
+++ src/main/java/bg/fmi/chatserver/chat/spi/ChannelRepository.java
@@ -1,7 +1,6 @@
 package bg.fmi.chatserver.chat.spi;
 
 import bg.fmi.chatserver.chat.Channel;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
